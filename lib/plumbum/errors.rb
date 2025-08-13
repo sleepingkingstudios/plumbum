21c26efdbda9b733f294d584a82dc66acab09c43
Rob Smith <sleepingkingstudios@gmail.com>
2025-08-13 15:04:24 -0400

    Implement Errors::InvalidKeyError.

--- lib/plumbum/errors.rb
+++ lib/plumbum/errors.rb
@@ -6,6 +6,7 @@ module Plumbum
   # Namespace for exceptions raised when handling Plumbum errors.
   module Errors
     autoload :ImmutableError,         'plumbum/errors/immutable_error'
+    autoload :InvalidKeyError,        'plumbum/errors/invalid_key_error'
     autoload :MissingDependencyError, 'plumbum/errors/missing_dependency_error'
   end
 end
